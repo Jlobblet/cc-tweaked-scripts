1dd6b57b283c4f6a20f98f1bb4e9d626d16d2eb8
John Blundell <jlobblet+github@jlobblet.co.uk>
2022-08-24 18:37:03 +0100

    Add colour profile

--- draw.lua
+++ draw.lua
@@ -1,7 +1,35 @@
 local monitor = assert(peripheral.find("monitor"))
-local input = arg[1]
+local imagePath = arg[1]
+local colourPath = arg[2]
 
-local image = assert(paintutils.loadImage(input))
+local colourFile = assert(io.open(colourPath, "r"))
+local colours = {
+    colours.white,
+    colours.orange,
+    colours.magenta,
+    colours.lightBlue,
+    colours.yellow,
+    colours.lime,
+    colours.pink,
+    colours.grey,
+    colours.lightGrey,
+    colours.cyan,
+    colours.purple,
+    colours.blue,
+    colours.brown,
+    colours.green,
+    colours.red,
+    colours.black,
+}
+
+for i = 1, #colours do
+    local line = colourFile:read("l")
+    local hex = tonumber(line, 16)
+    monitor.setPaletteColour(colours[i], hex)
+end
+
+colourFile:close()
+
 term.redirect(monitor)
+local image = assert(paintutils.loadImage(imagePath))
 paintutils.drawImage(image, 0, 0)
-term.setBackgroundColour(colours.black)
